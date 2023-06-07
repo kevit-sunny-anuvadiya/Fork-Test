6eccb743c39f93fa547b41a5daffcae5f9b7bf58
Dharmit Lakhani <dharmit.lakhani@kevit.io>
2023-06-07 13:03:34 +0530

    add multiply function

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const { multiply } = require("./multiply");
+
 function welcome() {
-    console.log('Welcome to git forking');
+  console.log("Welcome to git forking");
 }
-function add (number1 , number2) {
-    return number1 + number2;
+function add(number1, number2) {
+  return number1 + number2;
 }
+
+console.log(multiply(2, 3));
